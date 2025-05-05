1acdf2095fbfd30c8999f1d8ff3713115694b911
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-06 03:40:24 +0600

    Add Vue router configuration

---
+++ resources/js/routes/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+];
+
+export default routes;
