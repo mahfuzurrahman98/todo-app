f2602e33b3887021b8c2c4fdadd542681c194e11
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-06 00:29:42 +0600

    Fix AuthController logout method for proper error handling

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -2,14 +2,52 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Support\Facades\Response;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request) {}
+    public function login(LoginRequest $request)
+    {
+        try {
+            $email = $request->email;
+            $password = $request->password;
 
-    public function logout() {}
+            $user = User::where('email', $email)->first();
 
-    public function profile(Request $request) {}
+            if (!$user || !Hash::check($password, $user->password)) {
+                return Response::api('Invalid credentials', null,  401);
+            }
+
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            return Response::api('Login successful', [
+                'token' => $token,
+            ]);
+        } catch (\Exception $e) {
+            return Response::api($e->getMessage(), null, 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->currentAccessToken()->delete();
+            return Response::api('Logout successful');
+        } catch (\Exception $e) {
+            return Response::api($e->getMessage(), null, 500);
+        }
+    }
+
+    public function profile(Request $request)
+    {
+        try {
+            return Response::api('User profile fetched successfully', $request->user());
+        } catch (\Exception $e) {
+            return Response::api($e->getMessage(), null, 500);
+        }
+    }
 }
