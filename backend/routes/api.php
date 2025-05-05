5bffd72e5f5bc3c0f899ee53b4254015994f3b3b
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-05 12:52:29 +0600

    Add API routes for auth and todos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Auth routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/profile', [AuthController::class, 'profile']);
+
+// Todo resource routes
+Route::apiResource('todos', TodoController::class);
