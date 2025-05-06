90b4dc4612357f82f2d928caf78bb4a0de153b0c
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-06 06:02:19 +0600

    Update route configuration

--- resources/js/routes/index.ts
+++ resources/js/routes/index.ts
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import Login from "../pages/Login.vue";
+import { RouteRecordRaw } from "vue-router";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
     {
         path: "/",
         name: "Home",
