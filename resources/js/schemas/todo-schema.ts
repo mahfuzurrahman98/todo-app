cc7e256a4d6de55fd814f62b6b243485efea2673
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-07 01:36:07 +0600

    Add Todo validation schemas with Zod

---
+++ resources/js/schemas/todo-schema.ts
@@ -0,0 +1,20 @@
+import { boolean, date, object, string, union, infer as zInfer } from "zod";
+
+export const createTodoSchema = object({
+    title: string().min(1, { message: "Title is required" }),
+    body: string().optional(),
+    completed: boolean().default(false),
+});
+
+export const updateTodoSchema = createTodoSchema.partial();
+
+export const todoSchema = createTodoSchema.extend({
+    id: string(),
+    created_at: union([string(), date()]),
+    updated_at: union([string(), date()]),
+});
+
+
+export type CreateTodo = zInfer<typeof createTodoSchema>;
+export type UpdateTodo = zInfer<typeof updateTodoSchema>;
+export type Todo = zInfer<typeof todoSchema>;
