38a397705ad1ef62cf733d236a58539840cd000c
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-06 06:02:28 +0600

    Update Laravel routes configuration

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// API routes will go here
-
 // This catch-all route must be the last route defined
 // It returns the main view for all routes to be handled by Vue Router
 Route::get('/{any?}', function () {
