d4ded76c4e90fe736a4589a31ffa657454983705
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-06 06:02:11 +0600

    Update ErrorMessage component

--- resources/js/components/ErrorMessage.vue
+++ resources/js/components/ErrorMessage.vue
@@ -1,9 +1,11 @@
 <template>
-  <p v-if="message" class="text-sm font-medium text-red-500 mt-1">{{ message }}</p>
+    <p class="text-sm font-medium text-red-500 mt-1">
+        {{ message }}
+    </p>
 </template>
 
 <script setup lang="ts">
 defineProps<{
-  message?: string;
+    message?: string;
 }>();
 </script>
