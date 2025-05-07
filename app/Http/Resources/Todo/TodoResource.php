f4fc54c28cdfe68056041fb8a92cebfd9502f321
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-08 01:41:21 +0600

    refactor: update TodoResource on the backend

--- app/Http/Resources/Todo/TodoResource.php
+++ app/Http/Resources/Todo/TodoResource.php
@@ -19,6 +19,7 @@ class TodoResource extends JsonResource
             'title' => $this->title,
             'body' => $this->body,
             'completed' => $this->completed,
+            'user_id' => $this->user_id,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
