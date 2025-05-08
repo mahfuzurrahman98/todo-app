fc5c929d0dc2d66e5160690efb0782e93840c57b
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-09 00:44:10 +0600

    Update API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
+use App\Http\Middleware\CheckTodoOwnership;
 
 // Auth routes
 Route::prefix('auth')->group(function () {
@@ -15,5 +16,13 @@ Route::prefix('auth')->group(function () {
     });
 });
 
-// Todo resource routes
-Route::apiResource('todos', TodoController::class)->middleware('auth:sanctum');
+// Todo routes
+Route::prefix('todos')->middleware('auth:sanctum')->group(function () {
+    Route::post('/', [TodoController::class, 'store'])->name('todos.store');
+    Route::get('/', [TodoController::class, 'index'])->name('todos.index');
+    Route::middleware(CheckTodoOwnership::class)->group(function () {
+        Route::get('/{todo}', [TodoController::class, 'show'])->name('todos.show');
+        Route::put('/{todo}', [TodoController::class, 'update'])->name('todos.update');
+        Route::delete('/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
+    });
+});
