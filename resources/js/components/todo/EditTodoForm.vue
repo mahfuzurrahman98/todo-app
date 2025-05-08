113bca3fc599698c66cbfa570557d21e7e714179
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-08 10:59:04 +0600

    refactor: update EditTodoForm to match new component structure

--- resources/js/components/todo/EditTodoForm.vue
+++ resources/js/components/todo/EditTodoForm.vue
@@ -1,32 +1,30 @@
 <template>
     <div class="space-y-4">
-        <TodoForm :todo="todo" :is-submitting="isSubmitting" @submit="handleSubmit" @cancel="$emit('cancel')" />
+        <TodoForm
+            :todo="todo"
+            :is-submitting="isSubmitting"
+            @submit="handleSubmit"
+            @cancel="$emit('cancel')"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
-import { CreateTodo, Todo } from '../../schemas/todo-schema';
-import { TodoFormValue } from '../../types/todo';
-import TodoForm from './TodoForm.vue';
+import {
+    EditTodoFormEmits,
+    TodoItemProps as EditTodoFormProps,
+    TodoFormValue,
+} from "../../types/todo";
+import TodoForm from "./TodoForm.vue";
 
-interface EditFormProps {
-    todo: Todo;
-    isSubmitting?: boolean;
-}
-
-interface EditFormEmits {
-    (e: 'submit', id: number, updates: TodoFormValue): void;
-    (e: 'cancel'): void;
-}
-
-const props = defineProps<EditFormProps>();
-const emit = defineEmits<EditFormEmits>();
+const props = defineProps<EditTodoFormProps>();
+const emit = defineEmits<EditTodoFormEmits>();
 
 // Handle form submission
 const handleSubmit = (data: TodoFormValue) => {
-    emit('submit', props.todo.id, {
+    emit("submit", props.todo.id, {
         title: data.title,
-        body: data.body
+        body: data.body,
     });
 };
 </script>
