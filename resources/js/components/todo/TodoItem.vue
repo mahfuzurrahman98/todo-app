6b54649d4924696921c8676c3e60d8b909c76ae3
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-08 10:55:48 +0600

    style: reduce padding in TodoItem for more compact layout

--- resources/js/components/todo/TodoItem.vue
+++ resources/js/components/todo/TodoItem.vue
@@ -1,22 +1,33 @@
 <template>
-    <li class="px-6 py-4">
+    <li class="px-4 py-2">
         <!-- View Mode -->
-        <div v-if="!isEditing" class="flex items-center">
+        <div v-if="!isEditing" class="flex items-start">
             <!-- Checkbox -->
             <div class="mr-4">
-                <input :id="`todo-${todo.id}`" type="checkbox" :checked="todo.completed"
-                    @change="$emit('toggle-status', todo)" class="h-4 w-4 accent-gray-900" />
+                <input
+                    :id="`todo-${todo.id}`"
+                    type="checkbox"
+                    :checked="todo.completed"
+                    @change="$emit('toggle-status', todo)"
+                    class="h-4 w-4 accent-gray-900"
+                    :class="{
+                        'mt-1.5': todo.body,
+                        'mt-1': !todo.body,
+                    }"
+                />
             </div>
 
             <!-- Todo content -->
             <div class="flex-1 min-w-0" @click="startEditing">
                 <div class="block cursor-pointer">
-                    <p :class="[
-                        'text-sm font-medium',
-                        todo.completed
-                            ? 'text-gray-400 line-through'
-                            : 'text-gray-900',
-                    ]">
+                    <p
+                        :class="[
+                            'text-sm font-medium',
+                            todo.completed
+                                ? 'text-gray-400 line-through'
+                                : 'text-gray-900',
+                        ]"
+                    >
                         {{ todo.title }}
                     </p>
                     <p v-if="todo.body" class="text-sm text-gray-500 truncate">
@@ -26,45 +37,34 @@
             </div>
 
             <!-- Actions -->
-            <div class="ml-4 flex-shrink-0 flex space-x-2">
-                <button @click="startEditing"
-                    class="p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
-                    <PencilIcon class="h-5 w-5" />
-                </button>
-                <button @click="$emit('delete', todo)"
-                    class="p-1 rounded-full text-gray-400 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                    <TrashIcon class="h-5 w-5" />
-                </button>
-            </div>
+            <button
+                @click="$emit('delete', todo)"
+                class="p-1 rounded-full text-gray-400 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+            >
+                <TrashIcon class="h-5 w-5" />
+            </button>
         </div>
 
         <!-- Edit Mode -->
         <div v-else>
-            <EditTodoForm :todo="todo" :is-submitting="isSubmitting" @submit="handleSubmit" @cancel="cancelEditing" />
+            <EditTodoForm
+                :todo="todo"
+                :is-submitting="isSubmitting"
+                @submit="handleSubmit"
+                @cancel="cancelEditing"
+            />
         </div>
     </li>
 </template>
 
 <script setup lang="ts">
-import { PencilIcon, TrashIcon } from "lucide-vue-next";
-import { Todo } from "../../schemas/todo-schema";
-import { TodoFormValue } from "../../types/todo"
+import { TrashIcon } from "lucide-vue-next";
+import { TodoFormValue, TodoItemEmits, TodoItemProps } from "../../types/todo";
 import { ref } from "vue";
 import EditTodoForm from "./EditTodoForm.vue";
 
-interface ItemProps {
-    todo: Todo;
-    isSubmitting?: boolean;
-}
-
-interface ItemEmits {
-    (e: 'toggle-status', todo: Todo): void;
-    (e: 'update', id: number, updates: TodoFormValue): void;
-    (e: 'delete', todo: Todo): void;
-}
-
-const props = defineProps<ItemProps>();
-const emit = defineEmits<ItemEmits>();
+const props = defineProps<TodoItemProps>();
+const emit = defineEmits<TodoItemEmits>();
 
 // Edit mode state
 const isEditing = ref(false);
@@ -80,10 +80,10 @@ const cancelEditing = () => {
 };
 
 // Handle form submission
-const handleSubmit = (id: number, updates: TodoFormValue) => {
+const handleSubmit = (id: number, data: TodoFormValue) => {
     // Emit update event with todo ID and updated data
-    emit("update", id, updates);
-    
+    emit("update", id, data);
+
     // Exit edit mode
     isEditing.value = false;
 };
