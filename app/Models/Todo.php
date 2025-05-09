87bff6b4fae088adff1788d93d6d79bf0c9f9ec3
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-09 23:52:27 +0600

    Add HasFactory and default attributes to Todo model

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Todo extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'body',
         'completed',
         'user_id',
+    ];
+    
+    protected $attributes = [
+        'completed' => false,
     ];
 
     // define relationship
