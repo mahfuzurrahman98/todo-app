1148bc9e641efb70a0adecad94420482f42912dc
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2025-05-12 21:13:14 +0600

    refactor: add type hints to TodoRepositoryInterface methods

--- app/Repositories/Interfaces/TodoRepositoryInterface.php
+++ app/Repositories/Interfaces/TodoRepositoryInterface.php
@@ -5,8 +5,8 @@ namespace App\Repositories\Interfaces;
 interface TodoRepositoryInterface
 {
     public function getAll($userId = null);
-    public function findById($id);
+    public function findById(int $id);
     public function create(array $data);
-    public function update($id, array $data);
-    public function delete($id);
+    public function update(int $id, array $data);
+    public function delete(int $id);
 }
